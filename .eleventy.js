09c9ecde91affa7db03f79c8b64c166b881d7c7c
Catie1987 <hieu.nguyen912@gmail.com>
2024-08-08 16:05:25 +0700

    new update

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy("./src/header.js");
+    eleventyConfig.addPassthroughCopy("./src/bundle.js");
     eleventyConfig.addPassthroughCopy('./src/assets');
 
     eleventyConfig.addFilter("postDate", (dateObj)=>{

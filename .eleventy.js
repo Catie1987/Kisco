ca19a5dbf32f3139b3c1b1f94a56bbe90a2eb773
Catie1987 <hieu.nguyen912@gmail.com>
2024-08-09 16:20:14 +0700

    update lang

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/sub-page.css');
     eleventyConfig.addPassthroughCopy("./src/header.js");
     eleventyConfig.addPassthroughCopy("./src/bundle.js");
     eleventyConfig.addPassthroughCopy('./src/assets');
--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-
+    // General
     select_lang: {
       'en': 'EN',
       'vn': 'VN',
@@ -16,5 +16,132 @@ module.exports = {
       'en': 'Hello',
       'vn': 'Xin Chào',
       'jp': 'nihao'
-    }
+    },
+
+    // Navbar
+
+    nav1: {
+      'en': 'Home',
+      'vn': 'Trang Chủ',
+      
+    },
+    nav2: {
+      'en': 'About',
+      'vn': 'Giới thiệu',
+      
+    },
+    nav3: {
+      'en': 'Product & Service',
+      'vn': 'Sản phẩm & Dịch vụ',
+      
+    },
+    nav4: {
+      'en': 'News',
+      'vn': 'Tin tức',
+      
+    },
+    nav5: {
+      'en': 'Recruitment',
+      'vn': 'Tuyển dụng',
+      
+    },
+    nav6: {
+      'en': 'Contact us',
+      'vn': 'Liên hệ',
+      
+    },
+
+    // Homepage
+
+    welcome: {
+      'en': 'WELCOME TO KISCO (VIETNAM) LTD',
+      'vn': 'CHÀO MỪNG ĐẾN VỚI KISCO VIỆT NAM',
+      'jp': 'WELCOME TO KISCO (VIETNAM) LTD'
+    },
+    welcometext: {
+      'en': 'Kisco Vietnam is a subsidary of Kisco group, the global company with 49 branches from 19 countries.',
+      'vn': 'Kisco Việt Nam là công ty con của tập đoàn Kisco, công ty toàn cầu với 49 chi nhánh từ 19 quốc gia.',
+
+    },
+
+    welcometext2: {
+      'en': 'A global network that allows us to swiftly/accurately attain the lastest information and market trends regarding advanced material.',
+      'vn': 'Mạng lưới toàn cầu cho phép chúng tôi tiếp cận nhanh chóng/chính xác những thông tin mới nhất và xu hướng thị trường liên quan đến vật liệu tiên tiến.',
+
+    },
+
+    herobtn1: {
+      'en': 'Visit Us to know More',
+      'vn': 'Tìm Hiểu Thêm',
+      
+    },
+
+    sectiontitle1: {
+      'en': 'Product & Service',
+      'vn': 'Sản phẩm và Dịch vụ',
+      
+    },
+    sectiontext1: {
+      'en': 'Kisco Group create the ultimate values by aiming to provide the maximum satisfactions of our customers as integrated material solution providers. We always aim to provide optimal solutions by considering the quality, cost and environment.',
+      'vn': 'Tập đoàn Kisco tạo ra những giá trị tối ưu bằng cách hướng đến việc mang lại sự hài lòng tối đa cho khách hàng với tư cách là nhà cung cấp giải pháp vật liệu tích hợp. Chúng tôi luôn hướng đến việc cung cấp các giải pháp tối ưu bằng cách xem xét chất lượng, chi phí và môi trường.',
+
+    },
+
+    product1: {
+      'en': 'Electronic',
+      'vn': 'Điện Tử',
+    
+    },
+    product2: {
+      'en': 'Plastic & Rubber',
+      'vn': 'Nhựa & Cao Su',
+    
+    },
+    product3: {
+      'en': 'Parylene Coating',
+      'vn': 'Phủ Parylene',
+    
+    },
+    product4: {
+      'en': 'Chemical',
+      'vn': 'Hóa Chất',
+      
+    },
+    product5: {
+      'en': 'Life Science',
+      'vn': 'Khoa Học Đời Sống',
+      
+    },
+    product6: {
+      'en': 'Biotechnology',
+      'vn': 'Công Nghệ Sinh Học',
+      
+    },
+    sectiontitle2: {
+      'en': 'Our Offices',
+      'vn': 'Văn Phòng Chúng Tôi',
+      
+    },
+
+    sectiontext2a : {
+      'en': 'We have 2 offices in Vietnam and able to provide service all over country.',
+      'vn': 'Chúng tôi có 2 văn phòng tại Việt Nam và có thể cung cấp dịch vụ trên toàn quốc.',
+
+    },
+    sectiontext2b : {
+      'en': 'Please feel free and take you time to visit our office so we can discuss business and let us know more about you.',
+      'vn': 'Xin vui lòng dành thời gian ghé thăm văn phòng của chúng tôi để chúng ta có thể thảo luận về hoạt động kinh doanh và cho chúng tôi biết thêm về bạn.',
+
+    },
+    north: {
+      'en': 'NORTH',
+      'vn': 'MIỀN BẮC',
+
+    },
+    northlabel: {
+      'en': 'Hanoi Head Office',
+      'vn': 'Trụ sở chính Hà Nội',
+
+    },
+
   };
